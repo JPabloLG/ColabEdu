d0583b4e818828f2212baaf41c38b9a6dfbb4ace
JPabloLG <juanp.lopezg@uqvirtual.edu.co>
2025-04-22 18:42:16 -0500

    No sé por qué no se guardó
    Ahora si está la base bien

--- src/main/java/co/uniquindio/estructuras/colabedu/Controller/RegisterController.java
+++ src/main/java/co/uniquindio/estructuras/colabedu/Controller/RegisterController.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 
 import co.uniquindio.estructuras.colabedu.App;
 import co.uniquindio.estructuras.colabedu.Util.EmailService;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -26,16 +25,10 @@ public class RegisterController {
     private TextField txt_email;
 
     @FXML
-    private TextField txt_grade;
-
-    @FXML
     private TextField txt_name;
 
     @FXML
     private TextField txt_password;
-
-    @FXML
-    private TextField txt_school;
 
     @FXML
     private TextField txt_username;
@@ -62,12 +55,11 @@ public class RegisterController {
     void initialize() {
         assert txt_confirmPassword != null : "fx:id=\"txt_confirmPassword\" was not injected: check your FXML file 'RegisterView.fxml'.";
         assert txt_email != null : "fx:id=\"txt_email\" was not injected: check your FXML file 'RegisterView.fxml'.";
-        assert txt_grade != null : "fx:id=\"txt_grade\" was not injected: check your FXML file 'RegisterView.fxml'.";
         assert txt_name != null : "fx:id=\"txt_name\" was not injected: check your FXML file 'RegisterView.fxml'.";
         assert txt_password != null : "fx:id=\"txt_password\" was not injected: check your FXML file 'RegisterView.fxml'.";
-        assert txt_school != null : "fx:id=\"txt_school\" was not injected: check your FXML file 'RegisterView.fxml'.";
         assert txt_username != null : "fx:id=\"txt_username\" was not injected: check your FXML file 'RegisterView.fxml'.";
 
     }
 
 }
+
